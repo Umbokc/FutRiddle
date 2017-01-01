b1d80cc27737b7d628c72d52eb4a9f2052aa5e12
Umbokc <dr-stepan@mail.ru>
2017-01-01 17:36:20 +0300

    write level chars to unity

---
+++ Chars/AllChars.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllChars : MonoBehaviour {
+
+	public Transform all_chars;
+
+	private string[] levels_chars = {"JKVFPIXBGOAYCM","AAAAAAAAAAAAAA"};
+
+	void Start () {
+		int i = 0;
+		foreach(Transform child in all_chars){
+			
+			TextMesh TheChar = child.gameObject.GetComponentInChildren<TextMesh>();
+			TheChar.text = levels_chars[0][i].ToString();
+			
+			i++;
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
